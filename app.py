39f2b635c6f4da0ac10345310edbe0f533d7464e
wowba <87873821+wowba@users.noreply.github.com>
2021-12-27 18:27:23 +0900

    study

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-# Before Run, install Flask, requests, pymongo. It uses MongoDB
-
+# Before Run, install Flask, requests, pymongo, python-dotenv. It uses MongoDB
+import requests
 from flask import Flask, render_template, jsonify, request, session, redirect
 
-# dotenv 사용 연습 (.env 파일은 .gitignore로 항상 빼주기!)
+# dotenv 사용 연습 (.env 파일은 .gitignore 로 항상 빼주기!)
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-database = os.getenv("DATABASE")
-print(database)
+CLIENT_ID = os.getenv("CLIENTID")
+REDIRECT_URI = os.getenv("REDIRECTURI")
+KAKAO_SECRETKEY = os.getenv("KAKAOSECRETKEY")
 
 # Flask app 설정
 app = Flask(__name__)
 
-# session 시크릿 키
+# 임시 데이터 관리용 time
+import time
+
+# session 시크릿 키(실제로 사용 되는 서버는 .env 로 숨겨야 한다.)
 app.secret_key = 'VerySecretKey'
 
 from pymongo import MongoClient
 
-# localhost용 MongoDB 연결
+# localhost 용 MongoDB 연결
 client = MongoClient("localhost", 27017)
 
 # 서버용 MongoDB 연결
@@ -26,16 +30,15 @@ client = MongoClient("localhost", 27017)
 
 db = client.sideprojectlogin
 
-# 임시 데이터 관리용 time
-import time
 
-## HTML 화면 렌더링
+# HTML 화면 렌더링
 @app.route("/")
 def home():
     if "userID" in session:
-        return render_template("index.html",username = session.get("userID"),login = True,)
+        return render_template("index.html", username=session.get("userID"), login=True)
     else:
-        return render_template("index.html",login = False,)
+        return render_template("index.html", login=False, )
+
 
 @app.route("/join")
 def join():
@@ -44,12 +47,14 @@ def join():
     else:
         return render_template("join.html", login=False)
 
+
 @app.route("/write")
 def write():
     if "userID" in session:
         return render_template("write.html", username=session.get("userID"), login=True)
     else:
         return render_template("join.html", login=False)
+
 
 @app.route("/view")
 def view():
@@ -58,6 +63,7 @@ def view():
     else:
         return render_template("view.html", login=False)
 
+
 @app.route("/edit")
 def edit():
     if "userID" in session:
@@ -65,7 +71,8 @@ def edit():
     else:
         return render_template("view.html", login=False)
 
-## 유저 생성
+
+# 유저 생성
 
 @app.route("/join", methods=["POST"])
 def save_user():
@@ -75,39 +82,41 @@ def save_user():
     pw_receive = request.form["pw_give"]
 
     doc = {
-        "name":name_receive,
-        "email":email_receive,
-        "id":id_receive,
-        "pw":pw_receive
+        "name": name_receive,
+        "email": email_receive,
+        "id": id_receive,
+        "pw": pw_receive
     }
     print(doc)
     db.users.insert_one(doc)
-    
+
     return jsonify({"msg": "생성 완료"})
 
-## 로그인
 
-@app.route("/index",methods=["POST"])
+# 로그인
+
+@app.route("/index", methods=["POST"])
 def login():
     id_receive = request.form["id_give"]
     pw_receive = request.form["pw_give"]
 
     id_user = db.users.find_one({'id': id_receive})
     pw_user = db.users.find_one({'pw': pw_receive})
-    print("Login Check:",id_user["id"],pw_user["pw"])
-        ## id, pw 일치 확인
-    if id_user == None or pw_user == None:
+    print("Login Check:", id_user["id"], pw_user["pw"])
+    # id, pw 일치 확인
+    if id_user is None or pw_user is None:
         print("Wrong Account")
         return redirect("/")
     else:
         print("Correct Account")
-        ## 세션에 id 저장하기
+        # 세션에 id 저장하기
         session["userID"] = id_receive
-        ## 세션 확인
+        # 세션 확인
         print(session)
         return redirect("/")
 
-## 세션 삭제
+
+# 세션 삭제
 
 @app.route("/logout")
 def logout():
@@ -115,7 +124,7 @@ def logout():
     return redirect("/")
 
 
-## 게시글 작성
+# 게시글 작성
 
 @app.route("/write", methods=["POST"])
 def save_write():
@@ -125,28 +134,30 @@ def save_write():
     secs = str(time.time())
     print(secs)
 
-    ## 데이터베이스에 저장
+    # 데이터베이스에 저장
     doc = {
-        "time":secs,
-        "title":title_receive,
-        "content":content_receive,
-        "username":username
+        "time": secs,
+        "title": title_receive,
+        "content": content_receive,
+        "username": username
     }
     print(doc)
     db.noticeboard.insert_one(doc)
 
     return jsonify({"msg": "생성 완료"})
 
-## 게시글 표시
+
+# 게시글 표시
 
 @app.route("/show", methods=["GET"])
 def show_write():
-    noticeboards = list(db.noticeboard.find({},{"_id":False}))
-    idtest = db.noticeboard.find_one({})
-    print(idtest["_id"])
-    return jsonify({"all_noticeboards":noticeboards,})
+    noticeboards = list(db.noticeboard.find({}, {"_id": False}))
+    # idtest = db.noticeboard.find_one({})
+    # print(idtest["_id"])
+    return jsonify({"all_noticeboards": noticeboards, })
 
-## 게시글 삭제
+
+# 게시글 삭제
 
 @app.route("/delete", methods=["POST"])
 def delete_noticeboards():
@@ -154,7 +165,8 @@ def delete_noticeboards():
     print("모든 게시글 제거")
     return redirect("/")
 
-## 게시글 보기
+
+# 게시글 보기
 
 # 1. 해당 게시글의 id(time)값을 알아낸다.
 # 2. db에서 해당 게시글과 id가 동일한 데이터를 찾는다.
@@ -165,11 +177,12 @@ def delete_noticeboards():
 def show_noticeboard():
     id_receive = request.form["id_give"]
     print(id_receive)
-    board = db.noticeboard.find_one({"time":id_receive},{"_id":False})
+    board = db.noticeboard.find_one({"time": id_receive}, {"_id": False})
     print(board)
-    return jsonify({"board":board})
+    return jsonify({"board": board})
 
-## 게시글 수정
+
+# 게시글 수정
 
 # 1. 수정할 게시글의 id(time)값을 알아낸다.
 # 2. db에서 해당 게시글과 id가 동일한 데이터를 찾는다.
@@ -184,16 +197,42 @@ def edit_noticeboard():
     print(board)
     return jsonify({"board": board})
 
+
 @app.route("/save_edit", methods=["POST"])
 def save_edit():
     title_receive = request.form["title_give"]
     content_receive = request.form["content_give"]
     id_receive = request.form["id_give"]
-    db.noticeboard.update_one({'time': id_receive}, {'$set': {'title': title_receive,"content":content_receive}})
-    return jsonify({"msg":"수정 완료!"})
+    db.noticeboard.update_one({'time': id_receive}, {'$set': {'title': title_receive, "content": content_receive}})
+    return jsonify({"msg": "수정 완료!"})
 
+# 카카오 소셜 로그인 코드 받아오기
+@app.route("/code", methods=["GET"])
+def code():
+    code_url = f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    return redirect(code_url)
 
-## 포트
+# 코드를 이용해 토큰 받아오기
+@app.route("/oauth", methods=["GET"])
+def token():
+    CODE = request.args.get("code")
+    token_request = requests.post(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&code={CODE}&client_secret={KAKAO_SECRETKEY}")
+    token_json = token_request.json()
+    print(token_json)
+    access_token = token_json.get("access_token")
+    print(access_token)
+    # 유저 정보 가져오기
+    userdata = requests.get("https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"})
+    print(userdata.json())
+    return redirect("/")
+
+# 카카오 로그아웃
+@app.route("/kakao-logout")
+def kakao_logout():
+    # logout = requests.post("https://kapi.kakao.com/v1/user/logout", headers={"Authorization": f"Bearer {access_token}"})
+    return redirect("/")
+
+# 포트
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
